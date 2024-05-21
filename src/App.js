7f56d7169ab40e1694c23f4f63ac913fc81cd00a
vidya426 <vidya426@gmail.com>
2024-05-21 11:40:13 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import logo from './logo.svg';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Router } from 'react-router';
+import Login from './components/Login';
 
 function App() {
   return (
-   <div className='App'>
-    <h3>Build Sign up and Login UI Template in React</h3>
+   <Router>
+    <div className='auth-wrapper'>
+      <div className='auth-inner'>
+        <Routes>
+          <Route exact path="/" element={<Login/>}/>
+          <Route exact path="/sign-in" element={<Login/>}/>
+          <Route exact path="/sign-up" element={<Login/>}/>
+        </Routes>
+
+      </div>
     </div>
+   </Router>
   );
 }
 
