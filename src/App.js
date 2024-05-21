7c1824b772167f2eddb827654465b577a8927111
vidya426 <vidya426@gmail.com>
2024-05-21 12:30:03 +0100

    add login and signup forms

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import logo from './logo.svg';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Router } from 'react-router';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Login from './components/Login';
+import Signup from './components/Signup';
+import React from 'react';
 
 function App() {
   return (
    <Router>
+    <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={'/sign-in'}>
+              positronX
+            </Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-in'}>
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-up'}>
+                    Sign up
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
     <div className='auth-wrapper'>
       <div className='auth-inner'>
         <Routes>
           <Route exact path="/" element={<Login/>}/>
           <Route exact path="/sign-in" element={<Login/>}/>
-          <Route exact path="/sign-up" element={<Login/>}/>
+          <Route exact path="/sign-up" element={<Signup/>}/>
         </Routes>
-
+    </div>
       </div>
     </div>
    </Router>
---
+++ src/components/Signup.js
@@ -0,0 +1,12 @@
+import React from "react";
+function Signup(){
+    return(
+        <div>
+            <h3>
+                React Signup Component
+            </h3>
+        </div>
+    )
+}
+
+export default Signup;
